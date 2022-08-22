775186d807ce828b8a0bd226c423a62af3ea54dc
Navendu Duari <42625759+NavenduDuari@users.noreply.github.com>
2022-08-22 21:04:29 +0530

    Time: 82 ms (71.98%), Space: 44.2 MB (30.50%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -23,20 +23,52 @@
       // [1,2] [1,3]   [2,3]
       // [1,2,3]
 
-const backtrack = (nums, startIdx, sub, res) => {
-    res.push(sub);
-    for (let i = startIdx; i < nums.length; i++) {
-        sub.push(nums[i]);
-        backtrack(nums, i+1, [...sub], res);
-        sub.pop();
+// const backtrack = (nums, startIdx, sub, res) => {
+//     res.push(sub);
+//     for (let i = startIdx; i < nums.length; i++) {
+//         sub.push(nums[i]);
+//         backtrack(nums, i+1, [...sub], res);
+//         sub.pop();
+//     }
+// };
+
+// var subsets = function(nums) {
+//     const res = [];
+//     backtrack(nums, 0, [], res);
+    
+//     console.log('res::', res);
+    
+//     return res;
+// };
+
+// const backtrack = (nums, startIdx, subset, res) => {
+//     res.push([...subset]);
+//     for (let i = startIdx; i < nums.length; i++) {
+//         subset.push(nums[i]);
+//         backtrack(nums, i+1, subset, res);
+//         subset.pop();
+//     }
+// };
+
+// var subsets = (nums) => {
+//     const res = [];
+//     const subset = [];
+//     backtrack(nums, 0, subset, res);
+//     return res;
+// };
+
+const backtrack = (ip, startIdx, op, res) => {
+    res.push(op);
+    for (let i = startIdx; i < ip.length; i++) {
+        const num = ip[i];
+        const newOp = [...op, num];
+        backtrack(ip, i+1, newOp, res);
     }
 };
 
-var subsets = function(nums) {
+var subsets = (nums) => {
     const res = [];
-    backtrack(nums, 0, [], res);
-    
-    console.log('res::', res);
-    
+    const subset = [];
+    backtrack(nums, 0, subset, res);
     return res;
 };
