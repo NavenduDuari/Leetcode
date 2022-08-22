1e9009d579f52b223ad2efa0d1c063ac5571a2e2
Navendu Duari <42625759+NavenduDuari@users.noreply.github.com>
2022-08-23 01:17:29 +0530

    Time: 101 ms (30.57%), Space: 41.8 MB (93.19%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -33,25 +33,61 @@
 //     return nums[startIdx] === target ? startIdx : -1;
 // };
 
-var search = function(nums, target) {
-    const len = nums.length;
-    let startIdx = 0;
-    let endIdx = len-1;
+// var search = function(nums, target) {
+//     const len = nums.length;
+//     let startIdx = 0;
+//     let endIdx = len-1;
     
-    while (startIdx < endIdx) {
+//     while (startIdx < endIdx) {
+//         const midIdx = Math.floor((startIdx + endIdx) / 2);
+//         if (nums[midIdx] === target) return midIdx;
+//         // one of the two halves is monotonicaly increasing
+        
+//         // if left part is monotonically increasing then it is safe to drop the right part
+//         if (nums[startIdx] <= nums[midIdx]) { 
+//             if (target >= nums[startIdx] && target < nums[midIdx]) {
+//                 endIdx = midIdx - 1;
+//             } else {
+//                 startIdx = midIdx + 1;
+//             }
+//         } else { // right part is monotonically increasing
+//             if (target > nums[midIdx] && target <= nums[endIdx]) {
+//                 startIdx = midIdx + 1;
+//             } else {
+//                 endIdx = midIdx - 1;
+//             }
+//         }
+//     }
+    
+//     console.log('startIdx::', startIdx);
+    
+//     return nums[startIdx] === target ? startIdx : -1;
+// }
+
+// Modified binary search
+// 1. find the mid
+// 2. one part should be monotonic increasing(part1) and other(part2) would be NOT
+// 3. check if the target is between the part1
+// 4. if yes, then search on the part1
+// 5. if no, then search on the part2
+
+var search = function(nums, target) {
+    let startIdx = 0;
+    let endIdx = nums.length - 1;
+    
+    while (startIdx <= endIdx) {
         const midIdx = Math.floor((startIdx + endIdx) / 2);
         if (nums[midIdx] === target) return midIdx;
-        // one of the two halves is monotonicaly increasing
-        
-        // if left part is monotonically increasing then it is safe to drop the right part
-        if (nums[startIdx] <= nums[midIdx]) { 
-            if (target >= nums[startIdx] && target < nums[midIdx]) {
+            
+        // if left part is monotonic increasing
+        if (nums[startIdx] <= nums[midIdx]) {
+            if (nums[startIdx] <= target && target <= nums[midIdx]) {
                 endIdx = midIdx - 1;
             } else {
                 startIdx = midIdx + 1;
             }
-        } else { // right part is monotonically increasing
-            if (target > nums[midIdx] && target <= nums[endIdx]) {
+        } else { // right part is monotonic increamental
+            if (nums[midIdx] <= target && target <= nums[endIdx]) {
                 startIdx = midIdx + 1;
             } else {
                 endIdx = midIdx - 1;
@@ -59,7 +95,6 @@ var search = function(nums, target) {
         }
     }
     
-    console.log('startIdx::', startIdx);
+    return -1;
     
-    return nums[startIdx] === target ? startIdx : -1;
 }
